74703b5c6eaa08c567cc883324338702a5f315db
Phil Mach <110493891+philmach2@users.noreply.github.com>
2024-05-20 19:14:39 -0400

    landing page navbar

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -1,4 +1,5 @@
 import "@/assets/styles/globals.css";
+import Navbar from "@/components/Navbar";
 
 export const metadata = {
   title: "78 Charles | West Village Cooperative",
@@ -6,14 +7,15 @@ export const metadata = {
   keywords: "78, Charles, 78 Charles, west village, coop, property",
 };
 
-const MainLayout = ({ children }) => {
+const LandingLayout = ({ children }) => {
   return (
     <html lang="en">
       <body>
+        <Navbar />
         <div>{children}</div>
       </body>
     </html>
   );
 };
 
-export default MainLayout;
+export default LandingLayout;
--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,9 +1,12 @@
-const HomePage = () => {
+import Navbar from "@/components/Navbar";
+import Link from "next/link";
+
+const LandingPage = () => {
   return (
     <div>
-      <h1 className="text-3xl">Welcome</h1>
+      <h1 className="text-8xl">78 Charles</h1>
     </div>
   );
 };
 
-export default HomePage;
+export default LandingPage;
